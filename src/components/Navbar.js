7d4fa3d1ff2cfbc4ac5664ed6c450970f0c6e154
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-27 09:16:43 +0100

    Modify App.css file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
-export const Navbar = () => {
+export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scroll, setScroll] = useState(false);
 
@@ -21,12 +25,12 @@ export const Navbar = () => {
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
     };
-    
+
   return (
     <Navbar expand="lg" className={scroll ? 'scroll': ''}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="Logo" />
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
@@ -39,15 +43,9 @@ export const Navbar = () => {
           </Nav>
           <span className="Navbar-tex">
             <div className="social-icon">
-                <a href="#">
-                    <img src={''} alt=""/>
-                </a>
-                <a href="#">
-                    <img src={''} alt=""/>
-                </a>
-                <a href="#">
-                    <img src={''} alt=""/>
-                </a>
+                <a href="/"><img src={navIcon1} alt=""/></a>
+                <a href="/"><img src={navIcon2} alt=""/></a>
+                <a href="/"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log(connect)}><span/>Let's connect</button>
           </span>
