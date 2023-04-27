4a26e61cdc95f4e51cbca863097947fcf6f76676
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-27 10:55:25 +0100

    Add Banner.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/Navbar';
+import { Banner } from './components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <NavBar />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+export const Banner = () => {
+    return (
+        <section>
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
