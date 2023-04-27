a37055ad66e11a8a20457bafd7e123ac1ef8d139
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-27 12:11:35 +0100

    Install react-bootstrap-icons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,26 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap";
 
 export const Banner = () => {
-    return (
-        <section>
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
-}
+  return (
+    <section>
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`I’m a Software Developer`}
+              <span className="wrap">web developer</span>
+            </h1>
+            <p>
+              {" "}
+              I can help you build a product, feature or website Look through
+              some of my work and experience! If you like what you see and have
+              a project you need coded, don’t hestiate to contact me.
+            </p>
+            <button onClick={() => console.log('connect')}>Let's connect</button>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
