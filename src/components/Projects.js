bcc97562aaef47e1f8418dd9c1fc8ea59fa11a60
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-28 09:28:25 +0100

    Import Nav

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,47 @@
-const Projects = () => {
+import { Col, Container, Row, Nav } from "react-bootstrap";
 
-}
+const Projects = () => {
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <h2>Projects</h2>
+            <p>
+              Bringing ideas to life, one project at a time: A showcase of my
+              latest work.
+            </p>
+            <Nav variant="pills" defaultActiveKey="/home">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="third">
+                  Tab 3
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
 
 export default Projects;
