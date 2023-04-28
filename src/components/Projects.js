edb89457a06ba8d65bcf58afacc0b6a38f8dcd88
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-04-28 09:37:48 +0100

    Map project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Col, Container, Row, Nav } from "react-bootstrap";
+import { Col, Container, Row, Nav, Tab } from "react-bootstrap";
 
 const Projects = () => {
   const projects = [
@@ -24,19 +24,31 @@ const Projects = () => {
               Bringing ideas to life, one project at a time: A showcase of my
               latest work.
             </p>
-            <Nav variant="pills" defaultActiveKey="/home">
-              <Nav.Item>
-                <Nav.Link eventKey="first">Tab 1</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="second">Tab 2</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="third">
-                  Tab 3
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
+            <Tab.Container id="projects-tabs" defaultActiveKey="first">
+              <Nav variant="pills" defaultActiveKey="/home">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                    <Row>
+                        {projects.map((project, index) => {
+                            return (
+                                <p>{project.title}</p>
+                            )
+                        })
+                        }
+                    </Row>
+                </Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
           </Col>
         </Row>
       </Container>
