64366c54ba6f320f3ef70e0368ec32d229c3ab49
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-22 21:48:57 +0100

    Update projects.js file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,36 +8,39 @@ import projImg4 from "../assets/img/project4.png";
 import projImg5 from "../assets/img/project5.png";
 import projImg6 from "../assets/img/project6.png";
 import 'animate.css';
-
+import TrackVisibility from 'react-on-screen';
 
 
 const Projects = () => {
-  const projects = [
+  const firstProjects = [
     {
       title: "Marplex RTM",
       description: "Real Time Communication Project",
       imgUrl: projImg1,
     },
     {
-      title: "Landing Page",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Event Site",
+      description: "Event",
+      imgUrl: projImg5,
     },
+    
     {
       title: "Space Traveller's Hub",
       description: "Commercial and Scientific Space Travel Services",
       imgUrl: projImg3,
     },
+  ];
+
+  const secondProjects = [
      {
       title: "Cryptography",
       description: "Crypto Services",
       imgUrl: projImg4,
     },
-
     {
-      title: "Event Site",
-      description: "Event",
-      imgUrl: projImg5,
+      title: "Landing Page",
+      description: "Design & Development",
+      imgUrl: projImg2,
     },
      
     {
@@ -51,14 +54,17 @@ const Projects = () => {
     <section className="project" id="project">
       <Container>
         <Row>
-          <Col>
+          <Col size={12}>
+            <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
             <h2>Projects</h2>
             <p>
               Bringing ideas to life, one project at a time: A showcase of my
               latest work.
             </p>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" defaultActiveKey="/home">
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                 <Nav.Item>
                   <Nav.Link eventKey="first">Tab 1</Nav.Link>
                 </Nav.Item>
@@ -69,10 +75,23 @@ const Projects = () => {
                   <Nav.Link eventKey="third">Tab 3</Nav.Link>
                 </Nav.Item>
               </Nav>
-              <Tab.Content>
+              <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                 <Tab.Pane eventKey="first">
+                      <Row>
+                        {firstProjects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                <Tab.Pane eventKey="second">
                     <Row>
-                        {projects.map((project, index) => {
+                        {secondProjects.map((project, index) => {
                             return (
                                 <ProjectCard
                                 key={index}
@@ -84,9 +103,11 @@ const Projects = () => {
                     </Row>
                 </Tab.Pane>
                 <Tab.Pane eventKey="second">Math magic</Tab.Pane>
-                <Tab.Pane eventKey="third">Leaderboard</Tab.Pane>
+                <Tab.Pane eventKey="third">Problem Solving is key in Software development</Tab.Pane>
               </Tab.Content>
             </Tab.Container>
+            </div>}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
