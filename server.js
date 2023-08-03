95418c040b474f56fefaa07f001414de5b39f7a6
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-08-03 17:58:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
  
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
 // app.use(cors());
 // app.use(express.json());
